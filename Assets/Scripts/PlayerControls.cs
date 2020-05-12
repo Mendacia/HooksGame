e16f23974db02d465f0c1a078ab6ce42545c7e11
Liam <liamh2000@gmail.com>
2020-05-12 13:42:36 +1200

    Liam Testing Scene + Some Character Code

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public KeyCode left, right, jump;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //Basic Controls for Left and Right
+        if (Input.GetKey(right))
+        {
+            rb.velocity=new Vector2(5,0);
+        }
+
+        if (Input.GetKey(left))
+        {
+            rb.velocity=new Vector2(-5,0);
+        }
+
+
+        //Basic Jump Controls
+
+
+        //Hook Controls
+    }
+}
