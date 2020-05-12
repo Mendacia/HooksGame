aceb46fe3c0b55dd3ea5147dfb3749d58223bc60
Liam <liamh2000@gmail.com>
2020-05-12 14:51:22 +1200

    Player control clean up, and also added jumping.

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public PlayerControls player;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            player.SetGrounded(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            player.SetGrounded(false);
+        }
+    }
+}
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,30 +5,53 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     public KeyCode left, right, jump;
+    public bool isGrounded;
     private Rigidbody2D rb;
+    Vector2 wantedDirection;
 
     private void Start()
     {
+        //accessing the rigidbody
         rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        //Basic Controls for Left and Right
+        //Setting up variables for moving the player later based on inputs
+        var xIntent = 0;
+        var yIntent = rb.velocity.y;
+
+        //Taking inputs for LR player movement intent
         if (Input.GetKey(right))
         {
-            rb.velocity=new Vector2(5,0);
+            xIntent += 5;
         }
-
         if (Input.GetKey(left))
         {
-            rb.velocity=new Vector2(-5,0);
+            xIntent -= 5;
         }
 
+        //Taking inputs for the player's jump
+        if (isGrounded)
+        {
+            if (Input.GetKey(jump))
+            {
+                yIntent = 10;
+            }
+        }
 
-        //Basic Jump Controls
+        //Actually moving the player
+        wantedDirection = new Vector2(xIntent, yIntent);
+        rb.velocity = wantedDirection;
 
 
         //Hook Controls
+    }
+
+
+    //referenced in GroundCheck, basically referencing a trigger collider at the player's feet that tells the controller when the player is touching the ground
+    public void SetGrounded(bool grounded)
+    {
+        isGrounded = grounded;
     }
 }
