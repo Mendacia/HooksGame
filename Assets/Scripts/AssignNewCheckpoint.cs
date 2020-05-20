1135b1118cdb287a9410daac45a40a31c5da5f19
Liam <liamh2000@gmail.com>
2020-05-20 15:09:28 +1200

    Tutorial Text, Tutorial Level, and Analytics Events

--- Assets/Scripts/AssignNewCheckpoint.cs
+++ Assets/Scripts/AssignNewCheckpoint.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameAnalyticsSDK;
 
 public class AssignNewCheckpoint : MonoBehaviour
 {
     public GameObject spawnLocation;
+    public string checkpointName;
+    private bool triggeredBefore;
     private void OnTriggerStay2D(Collider2D collision)
     {
         var PlayerControls = collision.gameObject.GetComponent<PlayerControls>();
         if (PlayerControls != null)
         {
             PlayerControls.currentCheckpoint = spawnLocation.transform;
+            if(triggeredBefore == false)
+            {
+                GameAnalytics.NewProgressionEvent(GAProgressionStatus.Start, checkpointName);
+                triggeredBefore = true;
+            }
         }
     }
 }
