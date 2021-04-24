d5747217e3f015cae5611720e6df47a621c52489
Nemesia <liamh2000@gmail.com>
2021-04-24 18:53:36 +1200

    Speaky

---
+++ Assets/Scripts/Overhauled Scripts/ScriptableObjects/SpeakyHolder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Speaky Holder", menuName = "Hooks/Speaky Holder")]
+
+public class SpeakyHolder : ScriptableObject
+{
+    [TextArea(7, 7)]public string[] myLines;
+    [SerializeField] private int currentDialogue = 0;
+    private int thisLineCharacterCount = 0;
+
+    public void InitializeMe()
+    {
+        currentDialogue = 0;
+    }
+
+
+    public string SetNextText()
+    {
+        thisLineCharacterCount = 0;
+        foreach (char x in myLines[currentDialogue])
+        {
+            thisLineCharacterCount++;
+        }
+        currentDialogue++;
+        return (myLines[currentDialogue-1]);
+    }
+}
---
+++ Assets/Speaky.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speaky : MonoBehaviour
+{
+    [SerializeField] private GameObject textbox;
+    [SerializeField] private Text text;
+    private bool playerInRange = false;
+    public SpeakyHolder mySpeaky = null;
+
+    [SerializeField] private string fullText = null;
+    [SerializeField] private string displayedText = null;
+
+    private void Start()
+    {
+        mySpeaky.InitializeMe();
+    }
+
+    private void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            textbox.SetActive(true);
+            fullText = mySpeaky.SetNextText();
+            StartCoroutine(TextPopulator());
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            playerInRange = false;
+            displayedText = null;
+            fullText = null;
+            textbox.SetActive(false);
+        }
+    }
+    IEnumerator TextPopulator()
+    {
+        for(int i = 0; i < fullText.Length; i++)
+        {
+            displayedText = fullText.Substring(0, i+1);
+            text.text = displayedText;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+}
